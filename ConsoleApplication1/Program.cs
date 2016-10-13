e491685a174a13e025beaf65eb4a5a7d94503d5f
Stephens, Nick <NStephens8191@gmail.com>
2016-10-13 10:18:01 -0500

    Commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repository_To_Create;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(E.Test.GetDescription());
+            Console.ReadKey();
+        }
+    }
+}
--- Repository-To-Create/Class1.cs
+++ Repository-To-Create/Class1.cs
@@ -1,12 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repository_To_Create
 {
-    public class Class1
+    public static class EnumExtensions
     {
+        public static string GetDescription(this Enum enumValue)
+        {
+            // Get the field within the enum
+            var memberInfo = enumValue.GetType().GetMember(enumValue.ToString());
+
+            // If any members were found
+            if (memberInfo != null && memberInfo.Any())
+            {
+                // Get any description attributes on member
+                var descriptionAttributes =
+                    memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[];
+
+                if (descriptionAttributes != null && descriptionAttributes.Any())
+                {
+                    return descriptionAttributes.First().Description;
+                }
+            }
+
+            // If no member found or member does not have description attribute, return the default to string value
+            return enumValue.ToString();
+        }
+    }
+
+
+    public enum E
+    {
+        [Description("Hello World")]
+        Test
+    }
+
+    public static class Program1
+    {
+        public static void Main()
+        {
+            Console.Write(E.Test.GetDescription());
+        }
     }
 }
